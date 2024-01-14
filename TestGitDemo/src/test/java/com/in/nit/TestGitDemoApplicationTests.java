82d0be8fb7bf744591c67fc5d97f9e9631d64320
pratikmadekar <pratikmadekar88@gmail.com>
2024-01-14 16:34:12 +0530

    Setup completed

---
+++ TestGitDemo/src/test/java/com/in/nit/TestGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
